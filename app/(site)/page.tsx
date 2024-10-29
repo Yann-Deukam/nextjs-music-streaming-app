4857a1192f692b989c3160d4b03e1dd66f072dfa
Yann-Donald <yanndonald237@gmail.com>
2024-10-29 22:33:14 +0100

    Mobile navigation and main contant

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <h1 className="text-indigo-500">Main content</h1>;
+  return (
+    <h1 className="bg-neutral-900 rounded h-full w-full overflow-hidden overflow-y-auto">
+      <Header></Header>
+    </h1>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ChevronLeft, ChevronRight, Home, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface HeaderProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+const Header = ({ children, className }: HeaderProps) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    //handle logout
+  };
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-indigo-700 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <ChevronLeft size={32} />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <ChevronRight size={32} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <span className="text-black ">
+              <Home size={20} />
+            </span>
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <span className="text-black ">
+              <Search size={20} />
+            </span>
+          </button>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
