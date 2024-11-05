0319acf0e8cc5f26609b1b388ec3db4e6704b186
Yann-Donald <yanndonald237@gmail.com>
2024-11-05 12:08:29 +0100

    Song creation and upload

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 import React, { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -15,6 +16,7 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   );
 };
